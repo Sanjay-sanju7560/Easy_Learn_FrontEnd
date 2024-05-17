74642b6224c61ccd5d094cf1a417cbc3dbf92daa
Sanjay G <sanjaysanju7560@gmail.com>
2024-05-17 15:21:58 +0530

    backend deployed

--- src/Services/serverUrl.js
+++ src/Services/serverUrl.js
@@ -1,2 +1,2 @@
 // export const SERVER_URL = "http://localhost:3000"
-export const SERVER_URL = "https://pf-backend-nt3r.onrender.com/"
+export const SERVER_URL = "https://pf-backend-nt3r.onrender.com"
